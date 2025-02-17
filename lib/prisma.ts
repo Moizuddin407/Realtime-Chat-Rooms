47f4fd227118d731cd32d693da8879ff93e71f94
Moizuddin407 <moiz211j@gmail.com>
2025-02-18 01:42:43 +0500

    updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-const prisma = global.prisma || new PrismaClient()
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = global.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma
