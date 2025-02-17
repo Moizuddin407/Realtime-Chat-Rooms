eae76bc4daa431fd54390a6ade9835071f4eb398
Moizuddin407 <moiz211j@gmail.com>
2025-02-18 01:39:04 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil"
-    });
-    return config;
+  experimental: {
+    serverActions: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
