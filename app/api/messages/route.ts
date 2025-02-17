d3ccdc6be46c0010cfdaab1479d6fb5bc22dda2e
Moizuddin407 <moiz211j@gmail.com>
2025-02-18 01:25:43 +0500

    updated

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -2,7 +2,13 @@ import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import { broadcast } from '@/app/utils/broadcast'
 
-const prisma = new PrismaClient()
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export async function POST(req: Request) {
   try {
